9f62c83cd59a60dd6f1f7882b3fa0135172e72b9
Phước Phú <phuocphu414141@gmail.com>
2023-11-25 15:41:05 +0700

    update

--- src/pages/Orders/Components/EditItem.tsx
+++ src/pages/Orders/Components/EditItem.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { EditOutlined } from '@ant-design/icons'
-import { Button, Drawer, Form, Input, InputNumber, Modal, Select, Table, message } from 'antd'
+import { Button, Descriptions, DescriptionsProps, Divider, Drawer, Form, Input, InputNumber, Modal, Select, Table, message } from 'antd'
 import { axiosClient } from '../../../configs/axiosClient'
 import useAuthStore from '../../../store/UseAuthStore'
 type Props = {
@@ -59,6 +59,9 @@ export default function EditItem({ record, productsData, getData }: Props) {
             }
         }
     }
+    const handleChangeQuantity = () => {
+
+    }
 
     const handleDelete = async (data: any) => {
         newOrdersData = { 'orderDetails': ordersData.filter((item: any) => item.product.id !== data.product.id) }
@@ -98,30 +101,32 @@ export default function EditItem({ record, productsData, getData }: Props) {
         {
             title: 'Price',
             dataIndex: 'price',
-            key: 'price'
+            key: 'price',
+            align: 'right'
         },
         {
             title: 'Discount',
             dataIndex: 'discount',
-            key: 'discount'
+            key: 'discount',
+            align: 'center'
         },
         {
             title: 'Quantity',
             dataIndex: 'quantity',
             key: 'quantity',
+            align: 'center',
             render: (text: any, record: any, index: number) => {
                 return (
-                    <Form>
-                        <Form.Item>
-                            <InputNumber value={record.quantity} />
-                        </Form.Item>
-                    </Form>
+                    <>
+                        {record.quantity}
+                    </>
                 )
             }
         },
         {
             title: 'Total',
             key: 'Total',
+            align: 'right',
             render: (text: any, record: any, index: number) => {
                 return (
                     <>
@@ -133,6 +138,7 @@ export default function EditItem({ record, productsData, getData }: Props) {
         {
             title: '',
             key: 'actions',
+            width: 1,
             render: (text: any, record: any, index: number) => {
                 return (
                     <Button type='primary' danger
@@ -144,10 +150,39 @@ export default function EditItem({ record, productsData, getData }: Props) {
             }
         },
     ]
+    const items: DescriptionsProps['items'] = [
+        {
+            key: 1,
+            label: 'Order ID',
+            children: <>{record.id}</>
+        },
+        {
+            key: 1,
+            label: 'Status',
+            children: <>{record.status}</>
+        },
+        {
+            key: 1,
+            label: 'Customer',
+            children: <>{record.customer.firstName + record.customer.lastName}</>
+        },
+        {
+            key: 1,
+            label: 'Employee',
+            children: <>{record.employee.firstName + record.employee.lastName}</>
+        },
+        {
+            key: 1,
+            label: 'Description',
+            children: <>{record.description}</>
+        },
+    ]
     return (
         <>
             <Button onClick={handleClickBtn}><EditOutlined /></Button>
             <Drawer open={openModal} onClose={() => setOpenModal(false)} width='50%' title='Order Details'>
+                <Descriptions title='Order Details' bordered items={items} column={2} />
+                <Divider />
                 <Table columns={columns} dataSource={ordersData} />
                 <Form labelCol={{ span: 3 }} wrapperCol={{ span: 20 }} form={form} onFinish={addProductToOrder}>
                     <Form.Item label='Product' name='product'
